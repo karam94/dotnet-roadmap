74662279c3f0e03fcbd7135bd9df99ffc3276a81
Karam Kabbara <contact@karam.io>
2023-05-09 15:34:48 +0100

    Added section on classes, abstract classes, etc

---
+++ The Basics/C#/OOP/Classes/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Classes;
+
+public static class Program
+{
+    private static void Main()
+    {
+        Animal myDog = new Dog("Rufus");
+        myDog.Speak();
+
+        var result = Calculator.Multiply(3, 5);
+        Console.WriteLine($"3 multiplied by 5 is {result}");
+
+        var sum = Calculator.Add(1, 2, 3);
+        Console.WriteLine($"1 + 2 + 3 = {sum}");
+
+        var hypotenuse = Calculator.CalculateHypotenuse(10, 2);
+        Console.WriteLine(
+            $"The hypotenuse of a right angled triangle with one side 10cm & the other 2cm is {hypotenuse}");
+    }
+}
+
+public abstract class Animal
+{
+    protected readonly string Name;
+
+    protected Animal(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Speak();
+}
+
+public class Dog : Animal
+{
+    public Dog(string name) : base(name)
+    {
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine($"{Name} says woof!");
+    }
+}
+
+public class Cat : Animal
+{
+    public Cat(string name) : base(name)
+    {
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine($"{Name} says meow!");
+    }
+}
+
+public static class Calculator
+{
+    public static int Multiply(int x, int y) => x * y;
+    public static int Add(params int[] numbers) => numbers.Sum();
+
+    public static int CalculateHypotenuse(int a, int b)
+    {
+        static int Square(int z) => z * z;
+        return Square(a) + Square(b);
+    }
+}
---
+++ The Basics/C#/OOP/Classes/Tests/ProgramTests.cs
@@ -0,0 +1,98 @@
+using System;
+using System.IO;
+using Classes;
+using Xunit;
+
+namespace ClassesTests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void GivenDogCalledRufus_WhenSpeaks_ThenRufusSaysWoof()
+        {
+            // Arrange
+            Animal dog = new Dog("Rufus");
+            const string expectedOutput = "Rufus says woof!";
+
+            // Act
+            var consoleOutput = CaptureConsoleOutput(() => dog.Speak());
+
+            // Assert
+            Assert.Equal(expectedOutput, consoleOutput.Trim());
+        }
+
+        [Fact]
+        public void GivenCatCalledTiger_WhenSpeaks_ThenTigerSaysMeow()
+        {
+            // Arrange
+            Animal cat = new Cat("Tiger");
+            const string expectedOutput = "Tiger says meow!";
+
+            // Act
+            var consoleOutput = CaptureConsoleOutput(() => cat.Speak());
+
+            // Assert
+            Assert.Equal(expectedOutput, consoleOutput.Trim());
+        }
+
+        [Theory]
+        [InlineData(3, 0, 0)]
+        [InlineData(-2, 0, 0)]
+        [InlineData(-2, 2, -4)]
+        public void GivenCalculator_WhenMultiply_ThenReturnsExpectedResult(
+            int firstValue, int secondValue, int expectedResult)
+        {
+            // Act
+            var result = Calculator.Multiply(firstValue, secondValue);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(3, 5, 0, 8)]
+        [InlineData(3, 0, 0, 3)]
+        [InlineData(-2, 0, 1, -1)]
+        [InlineData(-2, 2, 10, 10)]
+        public void GivenCalculator_WhenAdd_ThenReturnsExpectedResult(
+            int firstValue, int secondValue, int thirdValue, int expectedResult)
+        {
+            // Act
+            var result = Calculator.Add(firstValue, secondValue, thirdValue);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(3, 3, 18)]
+        [InlineData(3, 1, 10)]
+        public void GivenCalculator_WhenCalculatingHypotenuse_ThenReturnsExpectedResult(
+            int firstValue, int secondValue, int expectedResult)
+        {
+            // Act
+            var result = Calculator.CalculateHypotenuse(firstValue, secondValue);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        private static string CaptureConsoleOutput(Action action)
+        {
+            var consoleOutput = new StringWriter();
+            var originalOut = Console.Out;
+
+            try
+            {
+                Console.SetOut(consoleOutput);
+                action();
+                return consoleOutput.ToString().Trim();
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+                consoleOutput.Dispose();
+            }
+        }
+    }
+}
